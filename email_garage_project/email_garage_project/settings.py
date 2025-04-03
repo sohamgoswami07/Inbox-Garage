590aa9f84cee12c5bcdcefb5d5c613f577dc0e08
Soham Goswami <sohamgoswami07@gmail.com>
2025-04-03 15:43:54 +0530

    databse connected, create admin

--- email_garage_project/email_garage_project/settings.py
+++ email_garage_project/email_garage_project/settings.py
@@ -102,12 +102,19 @@ WSGI_APPLICATION = 'email_garage_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if ENVIRONMENT == 'development':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.parse(env('DATABASE_URL'))
+    }
 
 
 # Password validation
