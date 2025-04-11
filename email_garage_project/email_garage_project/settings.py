f3d61ba52303ad4b63121b73b6e77fe62fabdc30
Soham Goswami <sohamgoswami07@gmail.com>
2025-04-11 23:40:46 +0530

    railway deployment testing

--- email_garage_project/email_garage_project/settings.py
+++ email_garage_project/email_garage_project/settings.py
@@ -58,6 +58,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+EXTERNAL_APPS = [
     'email_garage_app',
     'tailwind',
     'theme',
@@ -65,6 +68,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
 ]
+
+INSTALLED_APPS += EXTERNAL_APPS
 
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
