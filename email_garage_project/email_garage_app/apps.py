68fb49b6503fb70d80ad2743a462f08840adbabf
Soham Goswami <sohamgoswami07@gmail.com>
2025-04-19 12:56:47 +0530

    blog removed and names modified

--- email_garage_project/email_garage_app/apps.py
+++ email_garage_project/email_garage_app/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class EmailGarageAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'email_garage_app'
--- email_garage_project/email_garage_app/urls.py
+++ email_garage_project/email_garage_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('brand/', views.brand, name='brand'),
     path('templates/', views.templates, name='templates'),
-    path('blogs/', views.blogs, name='blogs'),
+    # path('blogs/', views.blogs, name='blogs'),
     path('email/<id>/', views.email_detail, name='email_detail'),
-    path('blog/<id>/', views.blog_detail, name='blog_detail'),
+    # path('blog/<id>/', views.blog_detail, name='blog_detail'),
     path('brand/<id>/', views.brand_detail, name='brand_detail'),
 ]
